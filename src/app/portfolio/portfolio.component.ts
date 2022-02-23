d7fa93a230ca03b2bcc79c77c8a64696c2902d38
Rob Steinkopf <rob.steinkopf@gmail.com>
2022-02-22 21:40:15 -0500

    adds initial portfolio section

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  public allFilter:boolean = true;
+  public brandFilter:boolean = false;
+  public marketingFilter:boolean = false;
+  public socialFilter:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public click_all_filter() {
+    this.allFilter = true;
+    this.brandFilter = false;
+    this.marketingFilter = false;
+    this.socialFilter = false;
+  }
+
+  public click_brand_filter() {
+    this.allFilter = false;
+    this.brandFilter = true;
+    this.marketingFilter = false;
+    this.socialFilter = false;
+  }
+
+  public click_marketing_filter() {
+    this.allFilter = false;
+    this.brandFilter = false;
+    this.marketingFilter = true;
+    this.socialFilter = false;
+  }
+
+  public click_social_filter() {
+    this.allFilter = false;
+    this.brandFilter = false;
+    this.marketingFilter = false;
+    this.socialFilter = true;
   }
 
 }
